ce654b17f75c2fdee3a9a654fd95aca81041c2aa
Adadezer <adadezer@gmail.com>
2023-02-16 19:07:51 -0300

    feat(backend): :sparkles: criação da tabela de lançamentos do contas a pagar

---
+++ backend/src/controllers/postsContasPagarController.js
@@ -0,0 +1,22 @@
+const PostsContasPagarService = require('../services/postsContasPagarService');
+
+class PostsContasPagarController {
+  PostsContasPagarService = new PostsContasPagarService();
+
+  async postLancamentosContasPagar(req, res) {
+    const { dataInicio, dataFim, codBanco } = req.body;
+    // console.log('reqBody >>>>', req.body);
+    const postEntrys = await
+    this.PostsContasPagarService.postLancamentosContasPagar(dataInicio, dataFim, codBanco);
+
+    return res.status(200).json(postEntrys);
+  }
+
+  async getAllColumnsLancamentosContasPagar(_req, res) {
+    const getColumns = await this.PostsContasPagarService.getAllColumnsLancamentosContasPagar();
+
+    return res.status(200).json(getColumns);
+  }
+}
+
+module.exports = PostsContasPagarController;
